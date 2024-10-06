c0d3c36c90d92d9797c4314b462d7f7c06cd42b3
Jose Solis <jasolisdev@gmail.com>
2024-10-05 18:34:57 -0700

    added medical-term-app

---
+++ ls
@@ -0,0 +1,11 @@
+components
+lib
+LICENSE
+ls
+next.config.js
+package.json
+package-lock.json
+pages
+prettier.config.js
+public
+README.md
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/medical-term-app/:path*',
+        destination: '/medical-term-app/:path*' // This matches static files served from the public directory
+      }
+    ]
+  }
 }
